72f30ca70d21e0591d5365caf1b2396cca382188
Alexander Chabin <nibach@ya.ru>
2022-11-12 13:09:24 +0500

    Remove unused `fetch` utils

--- api/utils/get-cors-wrapper.ts
+++
@@ -1,5 +0,0 @@
-const corsWrapperLink = 'https://allorigins.hexlet.app/raw?disableCache=true&url=';
-
-export function getCorsWrapper(link: string) {
-    return `${corsWrapperLink}${encodeURIComponent(link)}`;
-}
