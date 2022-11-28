e20d28cac1846dd20ff3960f5eeb2aeb02e6fe90
Ivan Krasheninnikov <krash577388@mail.ru>
2022-11-28 19:21:59 +0500

    first layers

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -15,8 +15,13 @@ const cn = classNames.bind(styles);
 function Map() {
   const centerPosition = [56.838011, 60.597465];
 
-  const [positionsData, setPositionsData] = useState([]);
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [oknPositionsData, setOknPositionsData] = useState([]);
+  const [isOknDataLoaded, setIsOknDataLoaded] = useState(false);
+  const [isOknVisible, setIsOknVisible] = useState(true);
+
+  const [housesPositionsData, setHousesPositionsData] = useState([]);
+  const [isHousesLoaded, setIsHousesLoaded] = useState(false);
+  const [isHousesVisible, setIsHousesVisible] = useState(true);
 
   useEffect(() => {
     (async function init() {
@@ -28,40 +33,72 @@ function Map() {
     })();
   }, []);
 
-  const req = new XMLHttpRequest();
-
-  req.addEventListener("load", () => {
-    if (isDataLoaded) {
+  const reqOkn = new XMLHttpRequest();
+  reqOkn.addEventListener("load", () => {
+    if (isOknDataLoaded) {
       return;
     }
-    setIsDataLoaded(true);
-    setPositionsData(JSON.parse(req.responseText));
+    setIsOknDataLoaded(true);
+    setOknPositionsData(JSON.parse(reqOkn.responseText));
   });
+  reqOkn.open("GET", "http://51.178.191.76:1337/api/okns?populate=geometry");
+  reqOkn.send();
 
-  req.open("GET", "https://map.ekaterinburg.design/api/map");
-  req.send();
+  const reqHouses = new XMLHttpRequest();
+  reqHouses.addEventListener("load", () => {
+    if (isHousesLoaded) {
+      return;
+    }
+    // console.log(reqHouses.responseText);
+    setIsHousesLoaded(true);
+    setHousesPositionsData(JSON.parse(reqHouses.responseText));
+  });
+  reqHouses.open("GET", "http://51.178.191.76:1337/api/house?populate=geometry");
+  reqHouses.send();
 
   return (
-    <MapContainer
+    <span>
+      <button className="bbb" onClick={() => setIsHousesVisible(!isHousesVisible)}>Click me!</button>
+      <MapContainer
       center={centerPosition}
       scrollWheelZoom
       attributionControl={null}
       zoom={13}
       className={cn(styles.Map)}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Markers positionsData={positionsData} />
-    </MapContainer>
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {isHousesVisible ? <HousesMarkers housesPositionsData={housesPositionsData}/> : null}
+        <OknMarkers oknPositionsData={oknPositionsData}></OknMarkers>
+      </MapContainer>
+    </span>
   );
 }
 
-function Markers(positionsData) {
+function OknMarkers(oknPositionsData){
   const markers = [];
-  for (const p of positionsData.positionsData) {
-    const parsedCoords = p.coords.split(", ").map((x) => Number(x));
+  if (!oknPositionsData.oknPositionsData || !oknPositionsData.oknPositionsData.data)
+    return;
+  for (const p of oknPositionsData.oknPositionsData.data) {
+    const coords = p.attributes.geometry.coordinates;
     markers.push(
-      <Marker position={parsedCoords}>
-        <Popup>{p.name}</Popup>
+      <Marker position={[coords[1], coords[0]]}>
+        <Popup>{"okn"}</Popup>
+      </Marker>
+    );
+  }
+
+  return markers;
+}
+
+function HousesMarkers(housesPositionsData){
+  const markers = [];
+  if (!housesPositionsData.housesPositionsData || !housesPositionsData.housesPositionsData.data)
+    return;
+  for (const p of housesPositionsData.housesPositionsData.data) {
+    const coords = p.attributes.geometry.coordinates;
+    markers.push(
+      <Marker position={[coords[1], coords[0]]}>
+        <Popup>{"house"}</Popup>
       </Marker>
     );
   }
