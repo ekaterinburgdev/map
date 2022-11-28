df6dbd357d570cca49c73aa055b67dab5dec43e6
Ivan Krasheninnikov <krash577388@mail.ru>
2022-11-28 19:32:44 +0500

    fix linter

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 import { useEffect, useState } from "react";
 import L from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
@@ -17,7 +18,6 @@ function Map() {
 
   const [oknPositionsData, setOknPositionsData] = useState([]);
   const [isOknDataLoaded, setIsOknDataLoaded] = useState(false);
-  const [isOknVisible, setIsOknVisible] = useState(true);
 
   const [housesPositionsData, setHousesPositionsData] = useState([]);
   const [isHousesLoaded, setIsHousesLoaded] = useState(false);
@@ -53,56 +53,74 @@ function Map() {
     setIsHousesLoaded(true);
     setHousesPositionsData(JSON.parse(reqHouses.responseText));
   });
-  reqHouses.open("GET", "http://51.178.191.76:1337/api/house?populate=geometry");
+  reqHouses.open(
+    "GET",
+    "http://51.178.191.76:1337/api/house?populate=geometry"
+  );
   reqHouses.send();
 
   return (
     <span>
-      <button className="bbb" onClick={() => setIsHousesVisible(!isHousesVisible)}>Click me!</button>
+      <button
+        className="bbb"
+        onClick={() => setIsHousesVisible(!isHousesVisible)}
+      >
+        Click me!
+      </button>
       <MapContainer
-      center={centerPosition}
-      scrollWheelZoom
-      attributionControl={null}
-      zoom={13}
-      className={cn(styles.Map)}
+        center={centerPosition}
+        scrollWheelZoom
+        attributionControl={null}
+        zoom={13}
+        className={cn(styles.Map)}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {isHousesVisible ? <HousesMarkers housesPositionsData={housesPositionsData}/> : null}
-        <OknMarkers oknPositionsData={oknPositionsData}></OknMarkers>
+        {isHousesVisible ? (
+          <HousesMarkers housesPositionsData={housesPositionsData} />
+        ) : null}
+        <OknMarkers oknPositionsData={oknPositionsData} />
       </MapContainer>
     </span>
   );
 }
 
-function OknMarkers(oknPositionsData){
+function OknMarkers(oknPositionsData) {
   const markers = [];
-  if (!oknPositionsData.oknPositionsData || !oknPositionsData.oknPositionsData.data)
+  if (
+    !oknPositionsData.oknPositionsData ||
+    !oknPositionsData.oknPositionsData.data
+  )
     return;
   for (const p of oknPositionsData.oknPositionsData.data) {
     const coords = p.attributes.geometry.coordinates;
     markers.push(
       <Marker position={[coords[1], coords[0]]}>
-        <Popup>{"okn"}</Popup>
+        <Popup>okn</Popup>
       </Marker>
     );
   }
 
+  // eslint-disable-next-line consistent-return
   return markers;
 }
 
-function HousesMarkers(housesPositionsData){
+function HousesMarkers(housesPositionsData) {
   const markers = [];
-  if (!housesPositionsData.housesPositionsData || !housesPositionsData.housesPositionsData.data)
+  if (
+    !housesPositionsData.housesPositionsData ||
+    !housesPositionsData.housesPositionsData.data
+  )
     return;
   for (const p of housesPositionsData.housesPositionsData.data) {
     const coords = p.attributes.geometry.coordinates;
     markers.push(
       <Marker position={[coords[1], coords[0]]}>
-        <Popup>{"house"}</Popup>
+        <Popup>house</Popup>
       </Marker>
     );
   }
 
+  // eslint-disable-next-line consistent-return
   return markers;
 }
 
