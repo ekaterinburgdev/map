2e392da741c7b1b660bee8df08449812696e9446
i.krasheninnikov <i.krasheninnikov@skbkontur.ru>
2023-01-20 22:33:34 +0500

    add desigh code data

---
+++ common/data/designCode/designCode.ts
@@ -0,0 +1,69 @@
+﻿
+
+export class DesignCode {
+    private filtersNames?: string[];
+    private readonly objectsIdsByType: Map<string, string[]>
+    private readonly objectById: Map<string, any>;
+    private inputData?: any;
+    
+    constructor() {
+        this.objectsIdsByType = new Map<string, string[]>();
+        this.objectById = new Map<string, any>();
+    }
+    
+    public async getFilters(): Promise<string[]> {
+        if (this.filtersNames)
+            return this.filtersNames;
+        
+        if (!this.inputData)
+            await this.getAndSaveData();
+        
+        const set = new Set<string>();
+        
+        for (const e of this.inputData){
+            if (!e.type)
+                continue;
+            set.add(e.type);
+        }
+        this.filtersNames = Array.from(set);
+        return this.filtersNames;
+    }
+    
+    public async getObjects(type: string): Promise<string[]> {
+        if (this.objectsIdsByType[type])
+            return this.objectsIdsByType[type];
+        
+        if(!this.inputData)
+            await this.getAndSaveData();
+            
+        const resultIds = [];
+        for (const e of this.inputData){
+            if (e.type === type)
+                resultIds.push(e.id);
+        }
+        
+        this.objectsIdsByType[type] = resultIds;
+        return resultIds;
+    }
+    
+    public async getObject(id: string): Promise<any>{
+        if (this.objectById[id])
+            return this.objectById[id];
+
+        if(!this.inputData)
+            await this.getAndSaveData();
+        
+        for (const e of this.inputData){
+            if (e.id !== id)
+                continue;
+            this.objectById[id] = e;
+            return e;
+        }
+    }
+    
+    private async getAndSaveData() {
+        await fetch("https://map.ekaterinburg.design/api/map")
+            .then(x => x.json())
+            .then(x => this.inputData = x);
+    }
+}
