2c911b54aa1742ba58dbf2d551be98381fcf8dfc
hiba9201 <pelevinaa@yandex-team.ru>
2023-01-22 16:06:42 +0500

    husky

--- components/Model/Lines/MapData.tsx
+++ components/Model/Lines/MapData.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { PolylineProps } from 'react-leaflet';
 import { Line } from 'components/UI/Map/Line/Line';
-
 import { Point, Sizes } from 'components/UI/Map/Point';
+
 import { RED_LINE_MOCK } from './red-line-mock';
 
 export type LinesMapDataProps = {
@@ -15,30 +15,35 @@ export type LinesMapDataProps = {
 
 export function LinesMapData({ data }: LinesMapDataProps) {
     const {
-        lines = [{ positions: [RED_LINE_MOCK], color: '#E31E24' }],
-        points = [
-            {
-                position: [56.83508212290889, 60.61074932672987],
-                color: '#E31E24',
-                id: '123',
-            },
-        ],
+        lines =
+        [{ positions: [RED_LINE_MOCK], color: '#E31E24' }],
+        points = [{
+            position: [
+                56.83508212290889,
+                60.61074932672987,
+            ],
+            color: '#E31E24',
+            id: '123',
+        }],
     } = data;
 
     return (
         <>
-            {lines.map((line) => (
-                <Line positions={line.positions} color={line.color} />
-            ))}
-            {points.map((point) => (
-                <Point
-                    id={point.id}
-                    position={point.position}
-                    color={point.color}
-                    size={Sizes.S}
-                    key={String(point.position)}
-                />
-            ))}
+            {lines.map(
+                // eslint-disable-next-line react/no-array-index-key
+                (line, idx) => <Line positions={line.positions} color={line.color} key={idx} />,
+            )}
+            {points.map(
+                (point) => (
+                    <Point
+                        id={point.id}
+                        position={point.position}
+                        color={point.color}
+                        size={Sizes.S}
+                        key={String(point.position)}
+                    />
+                ),
+            )}
         </>
     );
 }
