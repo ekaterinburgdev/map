f3a4a88775aa093c19270dfc8e262ed6c6d32d09
Alexander Chabin <nibach@ya.ru>
2023-05-27 13:37:57 +0500

    Update favicon and metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,11 @@
-/* eslint-disable */
 import React from 'react';
-import Head from 'next/head';
+import dynamic from 'next/dynamic';
 
 import 'styles/globals.css';
 import 'ekb/style.css';
 
-type AppProps<PropsType extends object> = {
-    Component: React.ComponentType<PropsType>;
-    pageProps: PropsType;
-};
+export const App = dynamic(() => import('../components/App'), { ssr: false });
 
-function App({ Component, pageProps }: AppProps<any>) {
-    const siteTitle = 'Карта объектов Дизайн-кода Екатеринбурга';
-
-    return (
-        <>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+export default function Home() {
+    return <App />;
 }
-
-export default App;
