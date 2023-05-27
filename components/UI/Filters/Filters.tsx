4d354f8ba12af6d53cfa4a780e39af9fbb2c0b6c
Alexander Chabin <nibach@ya.ru>
2023-05-27 14:44:20 +0500

    Don't show `<Welcome>` after closing (#36)

--- components/UI/Filters/Filters.tsx
+++ components/UI/Filters/Filters.tsx
@@ -13,10 +13,12 @@ import { FILTERS_CONFIG } from './Filters.config';
 import { FilterConfigItem, FilterType } from './Filters.types';
 
 export function Filters() {
-    const [isWelcomeClosed, setIsWelcomeClosed] = useState(false);
+    // TODO Add to Redux after development modal window with project information
+    const [isWelcomeClosed, setIsWelcomeClosed] = useState(localStorage.getItem('is-welcome-closed') !== null);
 
     const onClose = useCallback(() => {
         setIsWelcomeClosed(true);
+        localStorage.setItem('is-welcome-closed', '');
     }, []);
 
     const dispatch = useDispatch();
