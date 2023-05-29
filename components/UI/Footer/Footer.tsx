9809eda6bebc4c4ae67bf0e43579202dff4afcf8
Daria <DaArtPetrova@mail.ru>
2023-05-29 14:10:15 +0500

    Fixed margins for top level elements (#37)

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import { ProjectsPanel, PROJECT_MAP, Theme } from 'ekb';
 
 export function Footer() {
-    return <ProjectsPanel activeProjectId={PROJECT_MAP.id} theme={Theme.DARK} />;
+    return (
+        <ProjectsPanel
+            activeProjectId={PROJECT_MAP.id}
+            theme={Theme.DARK}
+            style={{ left: '8px', bottom: '8px' }}
+        />
+    );
 }
