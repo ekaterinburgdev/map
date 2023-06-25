ae14981654a0cee6dfc0603af09ddccba91dac3f
Alexander Chabin <nibach@ya.ru>
2023-06-25 15:30:52 +0500

    Fix service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 
 import 'styles/globals.css';
@@ -7,5 +7,19 @@ import 'ekb/style.css';
 export const App = dynamic(() => import('../components/App'), { ssr: false });
 
 export default function Home() {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('/sw.js')
+                .then((serviceWorker) => {
+                    // eslint-disable-next-line no-console
+                    console.debug('Service Worker registered: ', serviceWorker);
+                })
+                .catch((error) => {
+                    console.error('Error registering the Service Worker: ', error);
+                });
+        }
+    }, []);
+
     return <App />;
 }
