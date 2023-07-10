1095cfaf29a0c7d6fc99be3f0d8124455abb9274
Alexander Chabin <nibach@ya.ru>
2023-07-10 12:33:54 +0500

    Remove unused files

--- public/notion-static/placemarks.json.d.ts
+++
@@ -1,6 +0,0 @@
-import type { MapItem } from '../../common/types/map-item';
-
-declare module 'public/notion-static/placemarks.json' {
-    declare const PlacemarkData: MapItem[];
-    export default PlacemarkData;
-}
