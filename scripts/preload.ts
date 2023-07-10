3e724b048bdcc864e9c3159607c5a52f34670059
Alexander Chabin <nibach@ya.ru>
2023-07-10 13:01:02 +0500

    Create folder if not exists

--- scripts/preload.ts
+++ scripts/preload.ts
@@ -1,19 +1,26 @@
-import { writeFile } from 'fs/promises';
+import { existsSync, mkdirSync } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
 import houseBaseDownload from '../common/data/base/houseBaseDownload';
 import designCodeDownload from '../common/data/designCode/designCodeDownload';
 import dtpDownload from '../common/data/dtp/dtpDownload';
 import oknDownload from '../common/data/okn/oknDownload';
 
+const ROOT = './public/map-objects/';
+
 async function saveJSON(downloaderFn, filename) {
     const data = await downloaderFn();
-    return writeFile(filename, JSON.stringify(data));
+    return writeFile(`${ROOT}${filename}`, JSON.stringify(data));
+}
+
+if (!existsSync(ROOT)) {
+    mkdirSync(ROOT);
 }
 
 Promise.all([
-    saveJSON(houseBaseDownload, './public/map-objects/house-base.json'),
-    saveJSON(designCodeDownload, './public/map-objects/design-code.json'),
-    saveJSON(dtpDownload, './public/map-objects/dtp.json'),
-    saveJSON(oknDownload, './public/map-objects/okn.json'),
+    saveJSON(houseBaseDownload, 'house-base.json'),
+    saveJSON(designCodeDownload, 'design-code.json'),
+    saveJSON(dtpDownload, 'dtp.json'),
+    saveJSON(oknDownload, 'okn.json'),
 ]).then(() => {
     // eslint-disable-next-line no-console
     console.log('All JSON data downloaded');
