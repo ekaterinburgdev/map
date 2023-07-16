dcfa0a4054e60d2dcd5e32f8c0c9c1f9fb74c2ec
Anton Akhatov <anton@gcor.ru>
2023-07-16 23:40:15 +0100

    update copyright

--- components/UI/Copyright/Copyright.tsx
+++ components/UI/Copyright/Copyright.tsx
@@ -1,25 +1,27 @@
 /* eslint-disable */
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { activeFilterSelector } from 'state/features/selectors';
 import styles from './Copyright.module.css';
+import { FilterType } from '../Filters/Filters.types';
 
 export function Copyright() {
+    const activeFilter = useSelector(activeFilterSelector);
+
     return (
         <div className={styles.copyright}>
-            {'© '}
-            <a
-                href="https://kontikimaps.ru/how-old/dataset?p=h-ekb"
-                target="_blank"
-                rel="noreferrer"
-            >
-                how-old-is-this.house
-            </a>
-            {' | '}
+            {activeFilter === FilterType.HouseAge && (
+                <a
+                    href="https://kontikimaps.ru/how-old/dataset?p=h-ekb"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    © how-old-is-this.house {' | '}
+                </a>
+            )}
+
             <a href="https://www.openstreetmap.org/" target="_blank" rel="noreferrer">
-                OpenStreetMap
-            </a>
-            {' | '}
-            <a href="https://ekaterinburg.design/" target="_blank" rel="noreferrer">
-                ekaterinburg.design
+                OSM
             </a>
         </div>
     );
