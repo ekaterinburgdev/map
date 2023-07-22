039b3ee47bc67f11108029dab103f9c9d1da3037
Alexander Chabin <nibach@ya.ru>
2023-07-22 15:27:16 +0500

    Remove welcome message

--- components/UI/LeftSidebar/LeftSidebar.tsx
+++ components/UI/LeftSidebar/LeftSidebar.tsx
@@ -1,22 +1,10 @@
-import React, { useCallback, useState } from 'react';
-import { Welcome } from 'components/UI/Welcome/Welcome';
+import React from 'react';
 import { Filters } from 'components/UI/Filters/Filters';
 import styles from './LeftSidebar.module.css';
 
 export function LeftSidebar() {
-    // TODO Add to Redux after development modal window with project information
-    const [isWelcomeClosed, setIsWelcomeClosed] = useState(
-        localStorage.getItem('is-welcome-closed') !== null,
-    );
-
-    const onClose = useCallback(() => {
-        setIsWelcomeClosed(true);
-        localStorage.setItem('is-welcome-closed', '');
-    }, []);
-
     return (
         <div className={styles.leftSidebar}>
-            {!isWelcomeClosed && <Welcome onClose={onClose} />}
             <Filters />
         </div>
     );
