7e147ef9e75cb893749e82889b90e4c857326a75
petrova_d <petrova_d@tochka.com>
2023-07-31 08:39:31 +0500

    actualize content of modal

--- components/UI/AboutProjectModal/AboutProjectModal.tsx
+++ components/UI/AboutProjectModal/AboutProjectModal.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from 'components/UI/AboutProjectModal/AboutProjectModal.module.css';
 import { Team } from 'components/UI/AboutProjectModal/Team';
 // import { Partners } from 'components/UI/AboutProjectModal/Partners';
 import { JoinUs } from 'components/UI/AboutProjectModal/JoinUs';
+import mapService from './svg/map-service.svg';
 
 type TNewModalProps = {
     isOpen: boolean;
@@ -16,12 +18,20 @@ export function AboutProjectModal({ isOpen, onClose }: TNewModalProps) {
         <>
             <div className={styles.aboutProjectModal}>
                 <div className={styles.aboutProjectModal__content}>
+                    <div className={styles.serviceName}>
+                        <Image src={mapService} alt="Карта Екатеринбурга" />
+                        <p>Карта <br /> Екатеринбурга</p>
+                    </div>
                     <h3 className={styles.h3}>Раскрываем город в деталях</h3>
                     <div className={styles.paragraphs}>
-                        <p className={styles.largeText}>Мы - Код Екатеринбурга. Сообщество независимых энтузиастов, которые хотят сделать Екатеринбург лучше.</p>
-                        <p className={styles.largeText}>Поиск, сбор и визуализация данных занимают огромное количество времени, а результат этой работы обычно никуда не публикуется.</p>
+                        <p className={styles.largeText}>Мы создаём единую базу данных о городе, визуализируем её и превращаем неочевидную информацию в понятную и доступную.</p>
                         <p className={styles.largeText}>Наша цель - разработать платформу, на которой будут собраны различные данные о городе: возраст домов, этажность, объекты культурного наследия, ДТП и много чего ещё.</p>
-                        <p className={styles.largeText}>Мы создаём единую базу данных о городе, визуализируя его и превращая неочевидную информацию в понятную и доступную.</p>
+                        <p className={styles.largeText}>Проект является полностью открытым. Кто угодно может его контрибьютить или форкнуть. Заходите на гитхаб.</p>
+                    </div>
+                    <h4 className={styles.h4}>Планы</h4>
+                    <div className={styles.paragraphs}>
+                        <p className={styles.largeText}>Это бета-версия сервиса. В будущем мы планируем добавить новые слои данных, показывать сразу несколько слоёв, разработать API, визуализировать все города в мире и много чего ещё. Чтобы ничего не пропустить подписывайтесь на телеграм-канал.</p>
+                        <p className={styles.largeText}>Если у вас есть предложения, что ещё добавить или улучшить, оставьте фидбек.</p>
                     </div>
                     <Team />
                     {/* <Partners /> */}
--- components/UI/AboutProjectModal/JoinUs.tsx
+++ components/UI/AboutProjectModal/JoinUs.tsx
@@ -6,7 +6,7 @@ export function JoinUs() {
     return (
         <div className={styles.joinUs}>
             <h4 className={styles.h4}>Присоединяйся к нам</h4>
-            <Button text="Написать" type={ButtonType.YELLOW} onClick={() => {}} link="https://tally.so#tally-open=wL9Vd1&tally-width=650&tally-overlay=1&tally-emoji-animation=none" />
+            <Button text="Стать частью команды или помочь проекту" type={ButtonType.YELLOW} onClick={() => {}} link="https://tally.so#tally-open=wL9Vd1&tally-width=650&tally-overlay=1&tally-emoji-animation=none" />
         </div>
     );
 }
