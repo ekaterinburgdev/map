0e91338d3de13c2ee521a5fe66b83d169da17a2b
Alexander Chabin <nibach@ya.ru>
2023-08-07 03:01:34 +0500

    Remove `;` in end of app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <title>Инфокарта Екатеринбурга</title>
             </Head>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </>
     );
 }
