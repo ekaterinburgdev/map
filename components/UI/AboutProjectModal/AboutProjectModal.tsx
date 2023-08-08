4c05373d8d35707ae68b0ddf1a24347e5dba8574
PetrovaDaria <daartpetrova@mail.ru>
2023-08-08 22:55:19 +0500

    add close to modal

--- components/UI/AboutProjectModal/AboutProjectModal.tsx
+++ components/UI/AboutProjectModal/AboutProjectModal.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import styles from 'components/UI/AboutProjectModal/AboutProjectModal.module.css';
 import { AboutProjectContent } from 'components/UI/AboutProjectModal/AboutProjectContent';
 import { AboutProjectContext } from 'components/providers/AboutProjectProvider';
+import { Close } from 'components/UI/Close';
 
 export function AboutProjectModal() {
     const { close, isOpened } = useContext(AboutProjectContext);
@@ -11,6 +12,7 @@ export function AboutProjectModal() {
     return (
         <>
             <div className={styles.aboutProjectModal}>
+                <Close close={close} />
                 <AboutProjectContent />
             </div>
             <div className={styles.backdrop} onClick={close} />
