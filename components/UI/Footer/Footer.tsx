833ce37ab63006e99b2cf78ffdd4b1c94af79369
Alexander Chabin <nibach@ya.ru>
2023-08-09 18:02:45 +0500

    Update logo styles on phone screen

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ProjectsPanel, PROJECT_MAP, PRODUCTION_PROJECTS, Theme } from 'ekb';
+import styles from './Footer.module.css';
 
 export function Footer() {
     return (
-        <ProjectsPanel
-            projects={[PROJECT_MAP, ...PRODUCTION_PROJECTS]}
-            activeProjectId={PROJECT_MAP.id}
-            theme={Theme.DARK}
-            style={{ left: '8px', bottom: '8px' }}
-        />
+        <div className={styles.footer}>
+            <ProjectsPanel
+                projects={[PROJECT_MAP, ...PRODUCTION_PROJECTS]}
+                activeProjectId={PROJECT_MAP.id}
+                theme={Theme.DARK}
+                style={{ left: '8px', bottom: '8px' }}
+            />
+        </div>
     );
 }
