5bc9cc66fb6c3960d48d714e858377c4b53766d6
Alexander Chabin <nibach@ya.ru>
2023-08-11 03:03:20 +0500

    Update `AboutProjectModal` a11y (#83)
    * Add aria-attributes
    
    * Close Modal by Escape key

--- components/UI/AboutProjectModal/AboutProjectModal.tsx
+++ components/UI/AboutProjectModal/AboutProjectModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import styles from 'components/UI/AboutProjectModal/AboutProjectModal.module.css';
 import { AboutProjectContent } from 'components/UI/AboutProjectModal/AboutProjectContent';
 import { AboutProjectContext } from 'components/providers/AboutProjectProvider';
@@ -7,15 +7,27 @@ import { Close } from 'components/UI/Close';
 export function AboutProjectModal() {
     const { close, isOpened } = useContext(AboutProjectContext);
 
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.key === 'Escape') close();
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [close]);
+
     if (!isOpened) return null;
 
     return (
         <>
-            <div className={styles.aboutProjectModal}>
+            <div className={styles.aboutProjectModal} role="dialog" aria-modal="true">
                 <Close close={close} />
                 <AboutProjectContent />
             </div>
-            <div className={styles.backdrop} onClick={close} />
+            <div className={styles.backdrop} onClick={close} aria-hidden />
         </>
     );
 }
