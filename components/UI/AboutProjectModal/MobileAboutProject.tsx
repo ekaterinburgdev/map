8aaffb31bf2d6c68d07dc0a7ca3d520422405b2c
Alexander Chabin <nibach@ya.ru>
2023-08-10 18:07:27 +0500

    Simplify modal layout

--- components/UI/AboutProjectModal/MobileAboutProject.tsx
+++ components/UI/AboutProjectModal/MobileAboutProject.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { useContext } from 'react';
 import { AboutProjectContent } from 'components/UI/AboutProjectModal/AboutProjectContent';
 import { AboutProjectContext } from 'components/providers/AboutProjectProvider';
-import styles from './AboutProjectModal.module.css';
 
 export function MobileAboutProject() {
     const { isOpened, close } = useContext(AboutProjectContext);
@@ -14,9 +13,7 @@ export function MobileAboutProject() {
                 <Sheet.Header />
                 <Sheet.Content>
                     <Sheet.Scroller>
-                        <div className={styles.aboutProjectModal__content_mobile}>
-                            <AboutProjectContent />
-                        </div>
+                        <AboutProjectContent />
                     </Sheet.Scroller>
                 </Sheet.Content>
             </Sheet.Container>
