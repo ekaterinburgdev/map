98c91046a7c2dcb086bef47186e5cc8fc6c72fee
Pasha <pashaomelekhin@gmail.com>
2023-11-30 01:18:53 +0500

    Added link to Design Code manual

--- components/Layers/Facade/Filter/FacadeFilter.tsx
+++ components/Layers/Facade/Filter/FacadeFilter.tsx
@@ -9,6 +9,12 @@ export function FacadeFilter() {
                 опрятное и&nbsp;единое оформление. В&nbsp;этом слое показаны дома, которые входят в&nbsp;первый
                 пояс. Чтобы посмотреть регламент оформления фасада, кликните на&nbsp;дом, а&nbsp;затем на&nbsp;зелёную кнопку скачивания.
             </p>
+            <p>
+                <Link
+                    href="https://ekaterinburg.design/files/ekaterinburg-city-centre-design-code.pdf"
+                    text="Подробнее о дизайн-кода в туристском центре"
+                />
+            </p>
         </div>
     );
 }
